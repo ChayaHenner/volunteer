d129c0b7d0028aef137d2e07aa70f3029da5c821
ChayaHenner <chayahenner@gmail.com>
2023-12-05 12:02:23 +0200

    task validation

--- validation/taskValidation.js
+++ validation/taskValidation.js
@@ -1,13 +1,12 @@
 exports.validTask = (_reqBody)=>{
     let joinSchema =Joi.object({
         title: Joi.string().min(2).max(99).required(),
-        info: Joi.string().min(2).max(99).required(),
+        info: Joi.string().min(2).max(1500).required(),
         address: Joi.string().min(2).max(99).required(),
         time: Joi.string().min(2).max(99).required(),
-        date_created: Joi.number().min(9).max(120).required(),
-        association:  Joi.string().max(1000).required(),
-        creator: Joi.string().min(2).max(99).required(),
-        executor: Joi.number().min(0).max(5).required(),
+        association:  Joi.string().required(),
+        creator: Joi.string().required(),
+        executor: Joi.string().required(),
         taken: Joi.boolean().required()
     })
 }
